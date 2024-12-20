f9b88a406c9c66da60fbd1d05564a7cbb784f124
Yan Ming Leung <joshualeung15@gmail.com>
2024-12-20 10:45:06 -0800

    Create html templates

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home():  # put application's code here
+    return render_template('home.html')
 
+def about('/about'):
+    return render_template('about.html')
+
+def contact('/contact'):
+    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run()
