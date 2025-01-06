ef19b4bfa4e95cad26dc46831f3fa80ab4ed1e66
Yan Ming Leung <joshualeung15@gmail.com>
2025-01-05 23:22:54 -0800

    Display project on home page

--- app.py
+++ app.py
@@ -2,10 +2,34 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+projects = [
+    {
+        "name": "Site Density Analysis in Minneapolis and Minnesota",
+        # "thumb": "img/.png",
+        # "hero": "img/.png",
+        "categories": ["Python", "PySpark"],
+        # "slug": "",
+        # "prod": "",
+    },
+    {
+        "name": "Material Work Order Tag Generator",
+        # "thumb": "img/.png",
+        # "hero": "img/.png",
+        "categories": ["Flask", "Python", "Gunicorn", "Podman", "HTML", "CSS"],
+        "slug": "web-application",
+    },
+    {
+        "name": "Data pipeline development and analysis for kiwifruit drop in Italy ",
+        "thumb": "img/kiwifruit.png",
+        "hero": "img/kiwifruit.png",
+        "categories": ["Python", "PySpark"],
+        "slug": "api-docs",
+    },
+]
 
 @app.route('/')
 def home():  # put application's code here
-    return render_template('home.html')
+    return render_template('home.html', projects = projects)
 
 @app.route('/about')
 def about():
